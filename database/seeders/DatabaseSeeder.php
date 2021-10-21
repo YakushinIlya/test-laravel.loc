7ebf77997cb6c82647318038f2b272a1773fa2d1
Илья <yakushinilya74@gmail.com>
2021-10-21 14:36:46 +0500

    Step 7

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
+        $this->call(PermissionRoleSeeder::class);
         $this->call(CategorySeeder::class);
     }
 }
