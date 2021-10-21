c7c11b2243a891cb88107e8b44bcb7c429fdf6c4
Илья <yakushinilya74@gmail.com>
2021-10-21 14:35:49 +0500

    Step 6

--- database/seeders/PermissionRoleSeeder.php
+++ database/seeders/PermissionRoleSeeder.php
@@ -8,7 +8,36 @@ use Illuminate\Database\Seeder;
 class PermissionRoleSeeder extends Seeder
 {
     protected $data = [
-
+        [
+            1 => 1,
+        ],
+        [
+            1 => 2,
+        ],
+        [
+            2 => 1,
+        ],
+        [
+            6 => 1,
+        ],
+        [
+            7 => 1,
+        ],
+        [
+            7 => 2,
+        ],
+        [
+            3 => 1,
+        ],
+        [
+            3 => 2,
+        ],
+        [
+            4 => 1,
+        ],
+        [
+            5 => 1,
+        ],
     ];
 
     /**
@@ -18,9 +47,11 @@ class PermissionRoleSeeder extends Seeder
      */
     public function run()
     {
-        PermissionRole::create([
-            'permission_id' => 1,
-            'role_id'       => 1,
-        ]);
+        foreach($this->data as $k => $v) {
+            PermissionRole::create([
+                'permission_id' => $k,
+                'role_id'       => $v,
+            ]);
+        }
     }
 }
